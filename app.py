d377c33bf41113079c4e635289a16fe07bc34fe7
Daniil-Romanov <115372513+Daniil-Romanov@users.noreply.github.com>
2023-10-20 21:02:40 +0300

    login and python

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, jsonify
+import indexes
+import plotly.express as pe
+
+app = Flask(__name__)
+
+
+@app.route('/')  # Главная страница (index.html)
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login')  # Страница с входом
+def login():
+    return render_template('login.html')
+
+
+@app.route('/reg')  # Страница с регистрацией
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/analysis')  # Страница с графиками/данными
+def analysis():
+    return render_template('analysis.html')
+
+
+@app.route('/help')  # Страница "about"
+def help():
+    return render_template('help.html')
+
+
+@app.route('/plot', methods=['POST'])  # через эту функцию строятся графики
+def plot():
+    # Сбор данных с формы
+    ind = request.form['ind']
+    type_ = request.form['type']
+    vendor = request.form['vendor']
+    if vendor == 'all_v':
+        vendor = None
+    model = request.form['model']
+    if model == 'all_m':
+        model = None
+    startdate = request.form['startdate']
+    enddate = request.form['enddate']
+
+    # Постройка графиков в зависимости от выбранного индекса
+    if ind == 'Ins_mov':
+        data = indexes.instant_moving_index_interval(type_, startdate, enddate, vendor, model)
+    elif ind == 'Ins_y-y':
+        data = indexes.instant_year_year_index_interval(type_, startdate, enddate, vendor, model)
+    elif ind == 'Cur_mov':
+        data = indexes.current_moving_index_interval(type_, startdate, enddate, vendor, model)
+    elif ind == 'Cur_y-y':
+        data = indexes.current_year_year_index_interval(type_, startdate, enddate, vendor, model)
+    elif ind == 'Long_mov':
+        data = indexes.long_moving_index_interval(type_, startdate, enddate, vendor, model)
+    elif ind == 'Long_y-y':
+        data = indexes.long_year_year_index_interval(type_, startdate, enddate, vendor, model)
+
+    fig = pe.line(x=data['date'], y=data['index'])
+
+    fig_json = fig.to_json()
+    return jsonify({'fig_json': fig_json})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
